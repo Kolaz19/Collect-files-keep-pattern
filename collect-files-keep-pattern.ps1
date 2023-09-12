9487b8ad922a9a5b2632af9fa0c9449a5e55c783
Kolaz19 <etc.nikolas@gmail.com>
2023-09-12 20:55:19 +0200

    Color

--- collect-files-keep-pattern.ps1
+++ collect-files-keep-pattern.ps1
@@ -1,17 +1,28 @@
-Write-Output "What common pattern do the files in the folders have?"
+Write-Host "What common pattern do the files in the folders have?"-ForegroundColor Yellow
 $patternToSearch = Read-Host 
 $patternToSearch = "*" + $patternToSearch + "*"
 
 $files = Get-Childitem -Path $PSScriptRoot -Recurse -Include $patternToSearch -File 
 
+# Exit when no file with pattern found
 if ($files.length -eq 0) {
-    Write-Output "No match in folder - EXITING" 
+    Write-Host "No match in folder - EXITING" -ForegroundColor DarkRed
     Read-Host
     exit
 }
 
+# List files that match pattern
+Write-Host "Files found with this pattern:"-ForegroundColor Yellow
 forEach ($f in $files) {
     Write-Host $f.Name  
 }
 
-$patternToSearch = Read-Host 
+Write-Host "`nContinue?(y/n)"-ForegroundColor Yellow
+$answerContinue = Read-Host 
+
+if ($answerContinue -ne "y" -or $answerContinue -ne "Y") {
+    exit
+}
+
+Write-Host "`nName of new directory:" -ForegroundColor Yellow
+$directoryName = Read-Host 
