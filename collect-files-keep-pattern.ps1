552a2172607aaad4fa32792b8df06599a2155623
Kolaz19 <etc.nikolas@gmail.com>
2023-09-19 22:15:27 +0200

    Refactoring

--- collect-files-keep-pattern.ps1
+++ collect-files-keep-pattern.ps1
@@ -1,13 +1,21 @@
-Write-Host "What common pattern do the files in the folders have?"-ForegroundColor Yellow
-$patternToSearch = Read-Host 
+function checkContinue {
+
+    $answerContinue = Read-Host "Continue? (y/n)"
+
+    if ($answerContinue -ne "y" -or $answerContinue -ne "Y") {
+	exit
+    }
+
+}
+
+$patternToSearch = Read-Host "What common pattern do the files in the folders have?"
 $patternToSearch = "*" + $patternToSearch + "*"
 
-$files = Get-Childitem -Path $PSScriptRoot -Recurse -Include $patternToSearch -File 
+$files = Get-Childitem -Path $PSScriptRoot -Recurse -Filter $patternToSearch -File 
 
 # Exit when no file with pattern found
 if ($files.length -eq 0) {
-    Write-Host "No match in folder - EXITING" -ForegroundColor DarkRed
-    Read-Host
+    Read-Host "No match in folder - EXITING" 
     exit
 }
 
@@ -17,55 +25,46 @@ forEach ($f in $files) {
     Write-Host $f.Name  
 }
 
-Write-Host "`nContinue?(y/n)"-ForegroundColor Yellow
-$answerContinue = Read-Host 
+checkContinue
 
-if ($answerContinue -ne "y" -or $answerContinue -ne "Y") {
-    exit
-}
+$newDirectoryName = Read-Host "Name of new directory:"
 
-Write-Host "`nName of new directory:" -ForegroundColor Yellow
-$directoryName = Read-Host 
-
-# Check if new folder already exists
-$newFolderName = ($PSScriptRoot + '\' + $directoryName)
-$checkFolder = Test-Path $newFolderName 
-
-# Create or get folder to work with it
-if ($checkFolder) {
-    $folder = Get-Item -Path $newFolderName
-} else {
-    $folder = New-Item -Path $newFolderName -ItemType Directory
-}
-
-Write-Host "`nWhat pattern in the filename do you want to preserve? (Powershell regex allowed)" -ForegroundColor Yellow
-$pattern = Read-Host 
+$pattern = Read-Host "What pattern in the filename do you want to preserve? (Powershell regex allowed)"
 
 # First check if all files have this pattern - otherwise exit
-[System.Collections.ArrayList]$patterns = @()
+[System.Collections.ArrayList]$foundPatterns = @()
 foreach ($file in $files) {
     $patternFound = $file.Name -match $pattern
     if (!$patternFound) {
-	Write-Host "`nNot all files have this pattern!"
-	exit
+    Read-Host "Not all files have this pattern! - EXITING" 
+    exit
    }
-   $null = $patterns.Add($matches[0])
+   $null = $foundPatterns.Add($matches[0])
 }
 # Write all Found patterns
-Write-Host "`nFound patterns:" -ForegroundColor Yellow
-$patterns
+Write-Host "Found patterns:" -ForegroundColor Yellow
+$foundPatterns
 
-Write-Host "`nContinue?(y/n)"-ForegroundColor Yellow
-$answerContinue = Read-Host 
+checkContinue
 
-if ($answerContinue -ne "y" -or $answerContinue -ne "Y") {
-    exit
+# Check if new folder already exists
+$newDirectoryNameFull = ($PSScriptRoot + '\' + $newDirectoryName)
+$checkFolder = Test-Path $newDirectoryNameFull 
+# Create or get folder to work with it
+if (!$checkFolder) {
+    $null = New-Item -Path $newDirectoryNameFull -ItemType Directory
 }
 
-Write-Host "`nWhat should the new file name be (First part of name before preserved pattern)?" -ForegroundColor Yellow
-$fileNamePrefix = Read-Host 
+$fileNamePrefix = Read-Host "What should the new file name be (First part of name before preserved pattern)?"
+$toLower = Read-Host "Should the pattern be lower case? (y/n)"
+
 $index = 0
 foreach ($file in $files) {
-    $file | Copy-Item -Destination ($newFolderName + '\' + $fileNamePrefix + ' ' + $patterns[$index] + $file.Extension)
+    if ($toLower -eq "y" -or $toLower -eq "Y") {
+	$patternToWrite = $foundPatterns[$index].ToString().ToLower()
+    }
+
+    $file | Copy-Item -Destination ($newDirectoryNameFull + '\' + $fileNamePrefix + ' ' + $patternToWrite + $file.Extension)
+    Write-Host "Copied: $file.BaseName " -ForegroundColor Yellow
     $index++
 }
